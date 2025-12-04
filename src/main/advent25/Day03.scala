7dfa1650798c7a2ff8b96d90443a01ca306097ec
Will Normand <mail@wnormand.co.uk>
2025-12-04 00:51:23 +0000

    Day 03

--- src/main/advent25/Day03.scala
+++ src/main/advent25/Day03.scala
@@ -1,11 +1,16 @@
+package advent25
+
 object Day03 extends App {
   
 
   def largestJoltage(input: String): Int =
     // The output joltage is a 2 digit number.  We should first find the biggest number in anything except the last digit, 
     // then the biggest to the right of that.
-    val bank: List[Int] = input.toSeq.map(_.toInt).toList
+    val bank: List[Int] = input.map(_.toString.toInt).toList
+    println("bank is " + bank)
+    
     val firstBattery = bank.dropRight(1).sorted.last
     val secondBattery = bank.drop(bank.indexOf(firstBattery) + 1).sorted.last
+    println(s"first $firstBattery, secondBattery $secondBattery")
     (firstBattery.toString() + secondBattery.toString()).toInt
 }
