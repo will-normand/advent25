8834b29d0cb4606992e0d13e1fb1f7757165497e
Will Normand <mail@wnormand.co.uk>
2025-12-04 00:51:23 +0000

    Day 03

--- src/test/advent25/Day03.test.scala
+++ src/test/advent25/Day03.test.scala
@@ -1,7 +1,9 @@
 import advent25.Day03
 
-class Day03Test extends munit.FunSuite {
+class Day03mest extends munit.FunSuite {
   test("joltage") {
     assert(Day03.largestJoltage("987654321111111") == 98)
+    assert(Day03.largestJoltage("811111111111119") == 89)
+    assert(Day03.largestJoltage("234234234234278") == 78)
   }
 }
