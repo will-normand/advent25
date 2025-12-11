b49df7949a78d8a7802fec17612c4f1277a8dd8f
Will Normand <mail@wnormand.co.uk>
2025-12-11 00:17:19 +0000

    Formatting

--- src/main/advent25/Day06.scala
+++ src/main/advent25/Day06.scala
@@ -1,14 +1,14 @@
 import advent25.utils.InputFileReader
 import scala.collection.mutable.ListBuffer
 object Day06 extends App {
-  
+
   val inputMatrix = parseInput("input06.txt")
   val problems = inputMatrix.transpose
 
   def part1() = problems.map(solveProblem).sum
   println("Part 1 is " + part1())
 
-  def solveProblem(problem: Vector[String]): Long = 
+  def solveProblem(problem: Vector[String]): Long =
     val numbers = problem.dropRight(1).map(_.toLong)
     problem.last match
       case "+" => numbers.sum
@@ -16,16 +16,20 @@ object Day06 extends App {
 
   problems.foreach(p => println(solveCephalopodProblem(p)))
 
-  def solveCephalopodProblem(problem: Vector[String]): Long = 
-    // This assumes that the numbers are left-alisgned.  Actually they are 
+  def solveCephalopodProblem(problem: Vector[String]): Long =
+    // This assumes that the numbers are left-alisgned.  Actually they are
     // randomly distributed in the input so we need to find their location.
     val inputNumbers = problem.dropRight(1)
     var location = 0;
     val cephlaNumbers = ListBuffer[Int]()
 
     while (true) {
-      val cephlaNumber = inputNumbers.map(s => if location >= s.length then "" else s(location).toString()).reduce((a, b) => a + b)
-      println(s"Location $location cephla number $cephlaNumber, inputNumbers $inputNumbers")
+      val cephlaNumber = inputNumbers
+        .map(s => if location >= s.length then "" else s(location).toString())
+        .reduce((a, b) => a + b)
+      println(
+        s"Location $location cephla number $cephlaNumber, inputNumbers $inputNumbers"
+      )
       if (!cephlaNumber.isEmpty()) then
         cephlaNumbers.append(cephlaNumber.toInt)
         location += 1
@@ -35,16 +39,14 @@ object Day06 extends App {
           case "*" => cephlaNumbers.product
         return result
     }
-      
-    
+
     val numbers = problem.dropRight(1).map(_.toLong)
     problem.last match
       case "+" => numbers.sum
       case "*" => numbers.product
 
-
   def parseInput(filename: String): Vector[Vector[String]] =
     val lines = InputFileReader(filename).getLines
     lines.map(_.split("\\s+").toVector).toVector
-  
+
 }
